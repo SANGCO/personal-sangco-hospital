0faebd44cbbf7e80839c35db349cd2f62f523863
SANGCO <nsg1213@gmail.com>
2021-01-12 16:47:26 +0900

    Initial commit

---
+++ src/main/java/dev/sangco/sangcohospital/SangcoHospitalApplication.java
@@ -0,0 +1,13 @@
+package dev.sangco.sangcohospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangcoHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangcoHospitalApplication.class, args);
+	}
+
+}
